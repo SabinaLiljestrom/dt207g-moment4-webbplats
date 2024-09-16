fcc6b6bd5e32c880c31e36a7da804d816f61d72f
Sabina Liljeström <sabina.liljestrom@hotmail.com>
2024-09-16 14:31:27 +0200

    skapat css för styling av formulär

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+async function registerUser(username, password) {
+    const response = await fetch('http://localhost:3000/api/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    const data = await response.json();
+    console.log(data);
+  }
+  
